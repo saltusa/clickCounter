8a302110dd1f0d99ec886278ffa0c2e6ea9612a3
彦 曲 <yanyanyan@hikos-MacBook-Air.local>
2015-04-14 23:38:18 +0800

    added random background color change

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -47,10 +47,26 @@ class ViewController: UIViewController {
     func incrementCount(){
         self.count++
         self.label.text = "\(self.count)"
+        toggleBackGround()
     }
     func decrementCount(){
         self.count--
         self.label.text = "\(self.count)"
+        toggleBackGround()
+    }
+    func toggleBackGround(){
+        self.view.backgroundColor = getRandomColor()
+    }
+    func getRandomColor() -> UIColor{
+        
+        var randomRed:CGFloat = CGFloat(drand48())
+        
+        var randomGreen:CGFloat = CGFloat(drand48())
+        
+        var randomBlue:CGFloat = CGFloat(drand48())
+        
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1.0)
+        
     }
 }
 
