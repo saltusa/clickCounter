c3ce605f318d612277435fd785d26510d90be6b0
彦 曲 <yanyanyan@hikos-MacBook-Air.local>
2015-04-14 13:13:00 +0800

    added Count up and Count down button, which will increment and decrement the number

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -10,16 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    var label:UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Label
+        var label = UILabel()
+        label.frame = CGRectMake(150,150,60,60)
+        label.text = "0"
+        
+        //associate label's view to the view hierarchy, otherwise nothing shows on screen
+        self.view.addSubview(label)
+        self.label = label
+        
+        //Count Up Button
+        var countUpButton = UIButton()
+        countUpButton.frame = CGRectMake(150,250,100,60)
+        countUpButton.setTitle("countUp", forState: .Normal)
+        countUpButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(countUpButton)
+        
+        countUpButton.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        //decrement Button
+        var countDownButton = UIButton()
+        countDownButton.frame = CGRectMake(150, 300, 100, 60)
+        countDownButton.setTitle("countDown", forState: .Normal)
+        countDownButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(countDownButton)
+        countDownButton.addTarget(self, action: "decrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func incrementCount(){
+        self.count++
+        self.label.text = "\(self.count)"
     }
-
-
+    func decrementCount(){
+        self.count--
+        self.label.text = "\(self.count)"
+    }
 }
 
