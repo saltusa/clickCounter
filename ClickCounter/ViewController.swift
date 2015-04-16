75177725bfa825bc85ecfea561c0d90ff61fd05c
彦 曲 <yanyanyan@hikos-MacBook-Air.local>
2015-04-16 21:39:05 +0800

    deleted the old way through editing in code. and redid the whole thing using storyboard

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -11,47 +11,26 @@ import UIKit
 class ViewController: UIViewController {
 
     var count = 0
-    var label:UILabel!
+    @IBOutlet var label:UILabel!
+    @IBOutlet var label2:UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Label
-        var label = UILabel()
-        label.frame = CGRectMake(150,150,60,60)
-        label.text = "0"
         
-        //associate label's view to the view hierarchy, otherwise nothing shows on screen
-        self.view.addSubview(label)
-        self.label = label
-        
-        //Count Up Button
-        var countUpButton = UIButton()
-        countUpButton.frame = CGRectMake(150,250,100,60)
-        countUpButton.setTitle("countUp", forState: .Normal)
-        countUpButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
-        self.view.addSubview(countUpButton)
-        
-        countUpButton.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
-        
-        //decrement Button
-        var countDownButton = UIButton()
-        countDownButton.frame = CGRectMake(150, 300, 100, 60)
-        countDownButton.setTitle("countDown", forState: .Normal)
-        countDownButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
-        self.view.addSubview(countDownButton)
-        countDownButton.addTarget(self, action: "decrementCount", forControlEvents: UIControlEvents.TouchUpInside)
-    
-    }
-    func incrementCount(){
+       }
+    @IBAction func incrementCount(){
         self.count++
         self.label.text = "\(self.count)"
+        self.label2.text = "\(self.count)"
+
         toggleBackGround()
     }
-    func decrementCount(){
+    @IBAction func decrementCount(){
         self.count--
         self.label.text = "\(self.count)"
+        self.label2.text = "\(self.count)"
         toggleBackGround()
     }
     func toggleBackGround(){
